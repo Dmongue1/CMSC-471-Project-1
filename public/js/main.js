313ad97eb3ed9dfccd884d3e2c9f71ce8e9e1a0a
cpetrie8 <48536237+cpetrie8@users.noreply.github.com>
2019-03-15 01:24:10 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -29,10 +29,11 @@ var playGame = function(algoW=1, skillW=2, algoB=1, skillB=2) {
     console.log('Game Over');
     console.log('White: algo=' + algoW + ' skill=' + skillW);
     console.log('Black: algo=' + algoB + ' skill=' + skillB);
+    console.log('Number of Turns: ' + game.history().length);
     if (game.in_stalemate() || game.in_draw()){ 
        //evaluateBoard(game.board(), 'w') === 0
        //game.in_stalemate() is an empty function, used board eval instead. Stalemate when only kings left, so eval=0
-       console.log('Stalemate');
+       console.log('Stalemate / Draw');
     } else if (game.turn() === 'w'){ //Because if the next turn is white, the final turn must've been black
        console.log('Black wins');
     } else {
